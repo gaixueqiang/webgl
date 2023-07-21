37acf5bcaca34b8eeee062ca025002f45475b8af
gaixueqiang <wai_1029@163.com>
2023-07-21 18:22:11 +0800

    feat: 星星分布（起步）

---
+++ src/components/start.vue
@@ -0,0 +1,15 @@
+<template>
+  <div ref="dom"></div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import * as THREE from 'three';
+import createThree from '@/hooks/createThree';
+const dom = ref(null);
+const { renderer, scene } = createThree();
+
+onMounted(() => {
+  dom.value.appendChild(renderer.domElement);
+});
+</script>
