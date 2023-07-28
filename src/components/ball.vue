44a814fcf8e5d449cc3682f5e57f05c02e4fd985
gaixueqiang <wai_1029@163.com>
2023-07-28 18:45:38 +0800

    feat: ball 初始化

---
+++ src/components/ball.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="dom"></div>
+</template>
+<script setup>
+import { onMounted, ref } from 'vue';
+import * as THREE from 'three';
+import createThree from '@/hooks/createThree';
+const dom = ref(null);
+const { renderer, scene, camera } = createThree();
+
+onMounted(() => {
+  dom.value.appendChild(renderer.domElement);
+});
+
+const geometry = new THREE.SphereGeometry(3, 30, 30);
+const material = new THREE.MeshStandardMaterial({
+  color: '#fff',
+});
+
+const ball = new THREE.Mesh(geometry, material);
+
+scene.add(ball);
+</script>
