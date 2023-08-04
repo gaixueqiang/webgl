6f0e41b8d8d181373ca55e92e8f96a2ea02ceaaf
gaixueqiang <wai_1029@163.com>
2023-08-04 18:14:41 +0800

    feat: 受力球

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <!-- <AutomobileExhibit></AutomobileExhibit> -->
   <!-- <StarArrange count="100"></StarArrange> -->
-  <Acanthosphere></Acanthosphere>
+  <!-- <Acanthosphere></Acanthosphere> -->
+  <Ball></Ball>
 </template>
 
 <script>
 import AutomobileExhibit from './components/AutomobileExhibit.vue';
 import StarArrange from '@/components/starArrange.vue';
 import Acanthosphere from '@/components/Acanthosphere.vue';
+import Ball from '@/components/Ball.vue';
 export default {
   name: 'App',
-  components: { AutomobileExhibit, StarArrange, Acanthosphere },
+  components: { AutomobileExhibit, StarArrange, Acanthosphere, Ball },
 };
 </script>
 <style lang="scss">
--- src/components/ball.vue
+++ src/components/ball.vue
@@ -4,20 +4,65 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 import * as THREE from 'three';
+import * as CANNON from 'cannon-es';
 import createThree from '@/hooks/createThree';
 const dom = ref(null);
-const { renderer, scene, camera } = createThree();
+const { renderer, scene, updateRenderWorld } = createThree();
+const world = new CANNON.World();
+world.gravity.set(0, -10, 0);
 
 onMounted(() => {
   dom.value.appendChild(renderer.domElement);
 });
 
-const geometry = new THREE.SphereGeometry(3, 30, 30);
+const light = new THREE.AmbientLight(0x404040, 10);
+scene.add(light);
+
+const geometry = new THREE.SphereGeometry(0.3, 30, 30);
 const material = new THREE.MeshStandardMaterial({
-  color: '#fff',
+  color: 'red',
+});
+const ball = new THREE.Mesh(geometry, material);
+scene.add(ball);
+ball.position.set(0, 10, 0);
+
+const ballShape = new CANNON.Sphere(0.3);
+const ballBody = new CANNON.Body({
+  mass: 1,
+  shape: ballShape,
+  position: new CANNON.Vec3(0, 10, 0),
+});
+world.addBody(ballBody);
+
+const planeGeometry = new THREE.PlaneGeometry(50, 50);
+const planeMaterial = new THREE.MeshStandardMaterial({
+  color: 'green',
+  // side: THREE.DoubleSide,
 });
 
-const ball = new THREE.Mesh(geometry, material);
+const planeShape = new CANNON.Plane(50, 50);
+const planeBody = new CANNON.Body({ mass: 0, shape: planeShape });
+planeBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+world.addBody(planeBody);
 
-scene.add(ball);
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotateX(-Math.PI / 2);
+scene.add(plane);
+
+const clock = new THREE.Clock();
+
+const renderWorld = () => {
+  const detail = clock.getDelta();
+  world.step(detail);
+  if (ball) {
+    ball.position.copy(ballBody.position);
+    ball.quaternion.copy(ballBody.quaternion);
+  }
+};
+
+window.addEventListener('click', () => {
+  ballBody.applyForce(new CANNON.Vec3(-100, 0, 10));
+});
+
+updateRenderWorld(renderWorld);
 </script>
